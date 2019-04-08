8d54f2c3473020499c3cff77c4389f2f42a25a25
Hannah Ingham <hannahingham@MacBooks-MacBook-Pro.local>
2019-04-07 19:55:04 -0700

    changes

--- README.md
+++ README.md
@@ -26,3 +26,6 @@ visit: http://localhost:4000
 
 #### Resources:
 [primsma tutorial](https://www.prisma.io/blog/how-to-wrap-a-rest-api-with-graphql-8bf3fb17547d)
+
+### GraphQL insights
+`parent` gives you access to the information retrieved right before the nested query, which is useful if you need to use that data in the next query, such as using the lat long of a park to find the weather. 
--- src/schema.js
+++ src/schema.js
@@ -4,35 +4,39 @@ const gql = String.raw;
 typeDefs = gql`
 type Query{
     parks: [Park!]!
-    park(parkCode: String): Park!
+    park(parkCode: String!): [ParkDetail!]
     stateParks(stateCode: String!): [Park!]
-    weather: Weather!
 }
 
 type Park {
+    name: String!
+    latLong: String!
+    parkCode: String!
+}
+
+
+type ParkDetail {
    id: ID!
    parkCode: String!
    states: String!
    latLong: String!
    description: String!
    name: String!
-   weather: Weather!
+   weather(first: Int!): Weather!
 }
 
 type Weather {
     summary: String!
-    data: [Daily!]
+    data(first: Int): [Daily!]
 }
 
 type Daily {
+    time: String!
     summary: String!
-    sunriseTime: Int!
     sunsetTime: Int!
-    moonPhase: Int!
-    precipIntensity: Int!
-    temperatureHigh: Int!
-    temperatureLow: Int!
-    cloudCover: Int!
+    temperatureHigh: Float!
+    temperatureLow: Float!
+    windSpeed: Float!
 }
 
 `;
